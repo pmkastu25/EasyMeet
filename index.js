bfc3b010d126e2c2980ceaab28565e2295e10001
pmkastu25 <pmkastu25@gmail.com>
2025-07-03 11:19:40 +0530

    Backend Structure Setup Done

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
-const express = require("express");
+import dotenv from "dotenv"
+dotenv.config();
+
+import express from "express"
+import {createServer} from "node:http"
+import mongoose from "mongoose"
+import cors from "cors";
+import { connectToSocket } from "./src/controllers/socketManager.js";
+
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello");
-})
+const server = createServer(app);
+const io = connectToSocket(server);
 
-app.listen(8000, (req, res) => {
+app.set("port", (process.env.PORT || 8000)) //same as local storage
+
+
+app.use(cors());
+app.use(express.json({limit: "40kb"})); // to reduce payload
+app.use(express.urlencoded({limit: "40kb", extended: true}));
+
+
+app.get("/", (req, res) => {
+    return res.json({"Hello":"World"})
+});
+
+
+const start = async ()=>{
+    const connectiondb = await mongoose.connect(`${process.env.MONGO_URL}`);
+
+    console.log(`Mongo Connected DB Host: ${connectiondb.connection.host}`)
+
+    server.listen(app.get("port"), () => {
     console.log("Listening on PORT 8000");
 })
+}
+
+start();
