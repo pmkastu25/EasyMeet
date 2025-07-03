7514189a06082bf84d9be031bf769a9e196dcb0a
pmkastu25 <pmkastu25@gmail.com>
2025-07-03 12:41:40 +0530

    login register controller setup done

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import {createServer} from "node:http"
 import mongoose from "mongoose"
 import cors from "cors";
 import { connectToSocket } from "./src/controllers/socketManager.js";
-
+import userRoutes from "./src/routes/users.routes.js"
 const app = express();
 
 const server = createServer(app);
@@ -18,7 +18,7 @@ app.set("port", (process.env.PORT || 8000)) //same as local storage
 app.use(cors());
 app.use(express.json({limit: "40kb"})); // to reduce payload
 app.use(express.urlencoded({limit: "40kb", extended: true}));
-
+app.use("/api/v1/users", userRoutes);
 
 app.get("/", (req, res) => {
     return res.json({"Hello":"World"})
