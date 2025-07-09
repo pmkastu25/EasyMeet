92db838ed69678fe7a8052c1d9174d523c10b7d5
pmkastu25 <pmkastu25@gmail.com>
2025-07-09 22:39:38 +0530

    Auth Completed Login and Register

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import LandingPage from './pages/landing';
+import Authentication from './pages/authentication.jsx';
+import { AuthProvider } from './contents/AuthContext.jsx';
 
 function App() {
   return (
    <>
     <Router>
+      <AuthProvider>
       <Routes>
         {/* <Route path="/home" /> */}
         <Route path="/" element={<LandingPage/>} />
+        <Route path="/auth" element={<Authentication />}/>
       </Routes>
+      </AuthProvider>
     </Router>
    </>
   );
